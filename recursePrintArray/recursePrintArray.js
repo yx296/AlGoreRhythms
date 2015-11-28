29aa70235f17d0d5017fcd19198834feff4f6f0a
Yilin Xu <yx296@nyu.edu>
2015-11-28 13:36:22 -0800

    recursePrintArray with index

--- recursePrintArray/recursePrintArray.js
+++ recursePrintArray/recursePrintArray.js
@@ -1,14 +1,25 @@
-var arr =  [1, 2, 3, 5, 6];
+var arr =  [1, 2, 3, 4, 5];
 
 
-function recursePrintArray(arr) {
-  if (arr.length === 0) {
+// function recursePrintArray(arr) {
+//   if (arr.length === 0) {
+//     return;
+//   }
+//   console.log(arr[0]);
+//   arr = arr.slice(1);
+//   recursePrintArray(arr);
+// }
+
+function recursePrintArray(arr, index) {
+  index = index || 0;
+  console.log(arr[index]);
+
+  if (index === arr.length - 1) {
     return;
   }
-  console.log(arr[0]);
-  arr = arr.slice(1);
-  recursePrintArray(arr);
+  recursePrintArray(arr, index + 1);
 }
+
 
 
 recursePrintArray(arr);
