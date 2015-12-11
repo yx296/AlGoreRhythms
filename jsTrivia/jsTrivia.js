95bdfa5a010e1e2619764cb967961dd6ff9689cf
Yilin Xu <yx296@nyu.edu>
2015-12-11 02:24:23 -0800

    Add polymorphism

--- jsTrivia/jsTrivia.js
+++ jsTrivia/jsTrivia.js
@@ -1,17 +1,70 @@
 // bind and call
 
-var AnswerPrototype = {
-  get: function fn1() {
-    return this.val;
-  }
-};
-
-var lifeAnswer = Object.create(AnswerPrototype);
-lifeAnswer.val = 42;
-console.log(lifeAnswer.get());
-
-var dessertAnswer = Object.create(AnswerPrototype);
-dessertAnswer.val = 3.14;
-console.log(dessertAnswer.get());
+function Answer (value) {
+  this._val = value;
+}
 
 
+Answer.prototype.get = function fn1() {
+  return this._val;
+}
+
+var lifeAnswer = new Answer(42);
+lifeAnswer.get();
+
+var dessertAnswer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var AnswerPrototype = {
+//   constructor: function fn0 (value) {
+//     this._val = value;
+//   },
+//   get: function fn1() {
+//     return this._val;
+//   }
+// };
+
+// var lifeAnswer = Object.create(AnswerPrototype);
+// lifeAnswer.constructor(42);
+// console.log(lifeAnswer.get());
+
+// var dessertAnswer = Object.create(AnswerPrototype);
+// dessertAnswer.constructor(3.14);
+// console.log(dessertAnswer.get());
+
+
+// var FirmAnswerPrototype = Object.create(AnswerPrototype);
+
+// FirmAnswerPrototype.get = function fn2 (argument) {
+//   return AnswerPrototype.get.call(this) + "!!";
+// };
+
+// var luckyAnswer = Object.create(FirmAnswerPrototype);
+// luckyAnswer.constructor(7);
+// console.log(luckyAnswer.get());
